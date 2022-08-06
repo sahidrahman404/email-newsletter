d3ee74a083246afaefc425b13d3aa79feef673ea
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-07 06:14:58 +0700

    add subscribe handler and /subscriptions route

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,28 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
