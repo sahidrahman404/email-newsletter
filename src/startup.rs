e2bf37e2c9ee5f37dc4f2473225da576e3e58e80
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-07 19:11:33 +0700

    add app_data to register a PgConnection

--- src/startup.rs
+++ src/startup.rs
@@ -3,14 +3,20 @@ use crate::routes::health_check;
 use crate::routes::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             // A new entry in our routing table for POST /subscriptions requests
             .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
