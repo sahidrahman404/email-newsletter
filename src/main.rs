21415e9320de5ed46e4c449acf84e958041eb4b3
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-07 16:34:45 +0700

    add configuration for database connection and application port

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 //! src/health_check.rs
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
