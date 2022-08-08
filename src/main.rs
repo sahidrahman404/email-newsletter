c68791919103fe7da208b136d6bfb117d5c14b2b
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 13:39:59 +0700

    move the get_subscriber and init_subscriber to the newsletter library
    We are not just cleaning up for aesthetic/readability reasons - we are moving those two functions to
    the newsletter library to make them available to our test suite!

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
 //! src/health_check.rs
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s event to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    // We are falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "newsletter".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
