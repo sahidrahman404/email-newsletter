97f0ce789302509b724dba18a630af6ba4495345
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 08:26:15 +0700

    add Arc<PgPool> to the function parameter using web::Data extractor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,4 +9,9 @@ pub struct FormData {
 // Let's start simple: we always return a 200 OK
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    // Retrieving a connection from the application state!
+    connection: web::Data<PgPool>,
+) -> HttpResponse {
 }
