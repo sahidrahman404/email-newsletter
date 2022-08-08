4bacc21f6ac99717eeec4c82c3fa08b1ffea292f
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 09:55:45 +0700

    fixup! fixup! initialise set_logger function

--- src/startup.rs
+++ src/startup.rs
@@ -4,15 +4,10 @@ use crate::routes::subscribe;
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Wrap the connection in a smart pointer
     let connection = web::Data::new(db_pool);
     // Capture `connection` from the surrounding environment
