ba3fee37f6777fa80be2fda02baec734ec3cfb4c
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 08:19:50 +0700

    add PgPool to the run function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 //! src/health_check.rs
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+
+    run(listener, connection_pool)?.await
 }
