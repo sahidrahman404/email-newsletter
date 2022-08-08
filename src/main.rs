dbac075448b639bae65002dff7c0ea562acc4f12
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 09:36:04 +0700

    fixup! initialise set_logger function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! src/health_check.rs
+use env_logger::Env;
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use sqlx::PgPool;
@@ -6,6 +7,10 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
