021ceb13b641b8e450efd4903b2fdf0dc4840f11
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-08 09:19:58 +0700

    add logger middleware

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@
 use crate::routes::health_check;
 use crate::routes::subscribe;
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -12,6 +13,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             // A new entry in our routing table for POST /subscriptions requests
             .route("/subscriptions", web::post().to(subscribe))
